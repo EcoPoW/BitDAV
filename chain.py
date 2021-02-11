1a691bdb05b3ef95517743e1491ff9be64e4081e
KJ <kernel1983@gmail.com>
2021-02-11 15:03:24 +0800

    invite and leave

--- chain.py
+++ chain.py
@@ -14,6 +14,7 @@ class GossipHandler(tornado.web.RequestHandler):
         self.finish('chain test')
 
     def post(self):
+
         self.finish('chain test')
 
 
@@ -30,3 +31,19 @@ class JoinApproveHandler(tornado.web.RequestHandler):
 
     def post(self):
         self.finish('chain test')
+
+class InviteHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.finish('<form method="POST"><input name="addr"/><input type="submit" value="Invite"/></form>')
+
+    def post(self):
+        global messages
+        addr = self.get_argument('addr')
+        self.finish({'addr':addr, 'messages': messages})
+
+class LeaveHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.finish('<form method="POST"><input type="submit" value="Leave"/></form>')
+
+    def post(self):
+        self.finish('leave test')
