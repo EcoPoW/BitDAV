1aa61cc72d1bf807dfdc28879a5b3b4f10a64e74
KJ <kernel1983@gmail.com>
2021-02-16 12:35:41 +0800

    folder meta

--- chunk.py
+++ chunk.py
@@ -10,22 +10,19 @@ import pprint
 # chunk.py / filename
 
 # will generate a meta for folder and file chunks
+max_chunk_size = 1024*1024*100
 
 # group 0
 # pc1 300M
 # pc2 1500M
-group0_quota = [1024*1024*300, 1024*1024*1500]
+group0_quota = [1024*1024*300, 1024*1024*1600]
 group0_device_no = len(group0_quota)
+group0_current_device_index = 0
+quota = group0_quota[group0_current_device_index]
 # group 1
 # pc3 200M
 # pc4 300M
 
-max_chunk_size = 1024*1024*100
-
-print(sys.argv)
-
-foldername = sys.argv[1]
-filename = sys.argv[2]
 
 def mt_combine(hash_list, algorithm):
     l = len(hash_list)
@@ -37,40 +34,52 @@ def mt_combine(hash_list, algorithm):
     return result
 
 folder_meta_data = {'type':'folder_meta', 'items':[]}
-with open(filename, 'rb') as f:
-    group0 = []
-    quota = group0_quota.pop(0)
-    chunks = []
-    filesize = 0
 
-    while True:
-        data = f.read(max_chunk_size)
-        if not data:
-            break
-        chunk_hash = hashlib.sha256(data).hexdigest()
-        chunk_size = len(data)
-        filesize += chunk_size
-        # print(chunk_hash, chunk_size)
-        chunks.append([chunk_hash, chunk_size, group0_device_no-len(group0_quota)])
-        # write file
-        if quota < len(data):
-            quota = group0_quota.pop(0)
+print(sys.argv)
 
-        quota -= len(data)
-        print(quota)
+foldername = sys.argv[1]
+for filename in sys.argv[2:]:
+    # filename = sys.argv[2]
+
+    with open(filename, 'rb') as f:
+        group0 = []
+        chunks = []
+        filesize = 0
+
+        while True:
+            data = f.read(max_chunk_size)
+            if not data:
+                break
+            chunk_hash = hashlib.sha256(data).hexdigest()
+            chunk_size = len(data)
+            filesize += chunk_size
+            # print(chunk_hash, chunk_size)
+            chunks.append([chunk_hash, chunk_size, group0_device_no-len(group0_quota)])
+            # write file
+            if quota < len(data):
+                group0_current_device_index += 1
+                quota = group0_quota[group0_current_device_index]
+
+            quota -= len(data)
+            print(quota)
 
 
-hash_list = mt_combine([c[0] for c in chunks], hashlib.sha256)
-while len(hash_list) > 1:
-    hash_list = mt_combine(hash_list, hashlib.sha256)
-merkle_root = hash_list[0][-1]
-pprint.pprint(hash_list)
+    hash_list = mt_combine([c[0] for c in chunks], hashlib.sha256)
+    while len(hash_list) > 1:
+        hash_list = mt_combine(hash_list, hashlib.sha256)
+    merkle_root = hash_list[0][-1]
+    pprint.pprint(hash_list)
 
 
-file_meta_data = [os.path.basename(filename), filesize, merkle_root, time.time(), chunks]
-pprint.pprint(file_meta_data)
-file_meta_json = json.dumps(file_meta_data).encode()
-file_meta_hash = hashlib.sha256(file_meta_json).hexdigest()
-print(file_meta_hash, len(file_meta_json))
-with open('pc1/meta/%s' % file_meta_hash, 'wb') as f:
-    f.write(file_meta_json)
+    file_meta_data = [os.path.basename(filename), merkle_root, filesize, time.time(), chunks]
+    folder_meta_data['items'].append(file_meta_data)
+    # pprint.pprint(file_meta_data)
+    # file_meta_json = json.dumps(file_meta_data).encode()
+    # file_meta_hash = hashlib.sha256(file_meta_json).hexdigest()
+    # print(file_meta_hash, len(file_meta_json))
+
+folder_meta_json = json.dumps(folder_meta_data).encode()
+folder_meta_hash = hashlib.sha256(folder_meta_json).hexdigest()
+with open('pc1/meta/%s' % folder_meta_hash, 'wb') as f:
+    f.write(folder_meta_json)
+print(folder_meta_hash, len(folder_meta_hash))
