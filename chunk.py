705a09c27515b66934ad3100446332689dd140b9
KJ <kernel1983@gmail.com>
2021-02-17 02:17:12 +0800

    new folder meta

--- chunk.py
+++ chunk.py
@@ -41,66 +41,70 @@ def chunks_to_partition(chunks, partition_free_sizes):
     left_free_sizes = []
     index = 0
     free_size = partition_free_sizes[index]
-    for chunk_hash, chunk_size in chunks:
+    for chunk in chunks:
+        chunk_hash = chunk[0]
+        chunk_size = chunk[1]
         if free_size < chunk_size:
             left_free_sizes.append(free_size)
             index += 1
             free_size = partition_free_sizes[index]
         free_size -= chunk_size
-        result[(chunk_hash, chunk_size)] = index
+        result[chunk] = index
+    left_free_sizes.append(free_size)
     return result, left_free_sizes
 
-print(sys.argv[2:])
-foldername = sys.argv[1]
-folder_meta_data = {'type':'folder_meta', 'name': foldername, 'items':[]}
-for filename in sys.argv[2:]:
-    file_chunks = []
-    with open(filename, 'rb') as f:
-        # group0 = []
-        filesize = 0
+if __name__ == '__main__':
+    print(sys.argv[2:])
+    foldername = sys.argv[1]
+    folder_meta_data = {'type':'folder_meta', 'name': foldername, 'items':[]}
+    for filename in sys.argv[2:]:
+        file_chunks = []
+        with open(filename, 'rb') as f:
+            # group0 = []
+            filesize = 0
 
-        while True:
-            data = f.read(MAX_CHUNK_SIZE)
-            if not data:
-                break
-            chunk_hash = hashlib.sha256(data).hexdigest()
-            chunk_size = len(data)
-            filesize += chunk_size
-            # print(chunk_hash, chunk_size)
-            # chunks.append([chunk_hash, chunk_size, group0_device_no-len(group0_quota)])
-            file_chunks.append([chunk_hash, chunk_size])
-            # write file
-            # if quota < chunk_size:
-            #     group0_current_device_index += 1
-            #     quota = group0_quota[group0_current_device_index]
+            while True:
+                data = f.read(MAX_CHUNK_SIZE)
+                if not data:
+                    break
+                chunk_hash = hashlib.sha256(data).hexdigest()
+                chunk_size = len(data)
+                filesize += chunk_size
+                # print(chunk_hash, chunk_size)
+                # chunks.append([chunk_hash, chunk_size, group0_device_no-len(group0_quota)])
+                file_chunks.append([chunk_hash, chunk_size])
+                # write file
+                # if quota < chunk_size:
+                #     group0_current_device_index += 1
+                #     quota = group0_quota[group0_current_device_index]
 
-            # quota -= chunk_size
-            # print('quota', group0_current_device_index, quota)
+                # quota -= chunk_size
+                # print('quota', group0_current_device_index, quota)
 
-    chunks_to_go, group0_quota_left = chunks_to_partition(file_chunks, group0_quota)
-    pprint.pprint(chunks_to_go)
-    chunks = []
-    for chunk_hash, chunk_size in file_chunks:
-        chunks.append([chunk_hash, chunk_size, chunks_to_go[(chunk_hash, chunk_size)]])
+        chunks_to_go, group0_quota_left = chunks_to_partition(file_chunks, group0_quota)
+        pprint.pprint(chunks_to_go)
+        chunks = []
+        for chunk_hash, chunk_size in file_chunks:
+            chunks.append([chunk_hash, chunk_size, chunks_to_go[(chunk_hash, chunk_size)]])
 
-    print('chunks', chunks, len(chunks))
-    hash_list = mt_combine([c[0:1] for c in chunks], hashlib.sha256)
-    while len(hash_list) > 1:
-        pprint.pprint(hash_list)
-        print('---')
-        hash_list = mt_combine(hash_list, hashlib.sha256)
-    merkle_root = hash_list[0][-1]
+        print('chunks', chunks, len(chunks))
+        hash_list = mt_combine([c[0:1] for c in chunks], hashlib.sha256)
+        while len(hash_list) > 1:
+            pprint.pprint(hash_list)
+            print('---')
+            hash_list = mt_combine(hash_list, hashlib.sha256)
+        merkle_root = hash_list[0][-1]
 
 
-    file_meta_data = [os.path.basename(filename), merkle_root, filesize, time.time(), chunks]
-    folder_meta_data['items'].append(file_meta_data)
-    # pprint.pprint(file_meta_data)
-    # file_meta_json = json.dumps(file_meta_data).encode()
-    # file_meta_hash = hashlib.sha256(file_meta_json).hexdigest()
-    # print(file_meta_hash, len(file_meta_json))
+        file_meta_data = [os.path.basename(filename), merkle_root, filesize, time.time(), chunks]
+        folder_meta_data['items'].append(file_meta_data)
+        # pprint.pprint(file_meta_data)
+        # file_meta_json = json.dumps(file_meta_data).encode()
+        # file_meta_hash = hashlib.sha256(file_meta_json).hexdigest()
+        # print(file_meta_hash, len(file_meta_json))
 
-folder_meta_json = json.dumps(folder_meta_data).encode()
-folder_meta_hash = hashlib.sha256(folder_meta_json).hexdigest()
-with open('pc1/meta/%s' % folder_meta_hash, 'wb') as f:
-    f.write(folder_meta_json)
-print('folder_meta_hash', folder_meta_hash, len(folder_meta_json))
+    folder_meta_json = json.dumps(folder_meta_data).encode()
+    folder_meta_hash = hashlib.sha256(folder_meta_json).hexdigest()
+    with open('pc1/meta/%s' % folder_meta_hash, 'wb') as f:
+        f.write(folder_meta_json)
+    print('folder_meta_hash', folder_meta_hash, len(folder_meta_json))
--- relocation_chunk.py
+++ relocation_chunk.py
@@ -6,6 +6,8 @@ import json
 import time
 import pprint
 
+from chunk import chunks_to_partition
+
 # put file folder / filename
 # chunk.py / filename
 
@@ -15,7 +17,8 @@ MAX_CHUNK_SIZE = 1024*1024*10
 # group 0
 # pc1 300M
 # pc2 1500M
-group0_quota = [1024*1024*300, 1024*1024*1600]
+# group0_quota = [1024*1024*300+12, 1024*1024*1600]
+group0_quota = [1000*1000*300+12, 1000*1000*1600]
 group0_device_no = len(group0_quota)
 group0_current_device_index = 0
 
@@ -26,38 +29,73 @@ quota = group0_quota[group0_current_device_index]
 # pc3 200M
 # pc4 300M
 
+if __name__ == '__main__':
+    print(sys.argv[2:])
+    folder_meta_hash = sys.argv[1]
+    folder_meta_json = open('pc1/meta/%s' % folder_meta_hash, 'rb').read()
+    folder_meta_data = json.loads(folder_meta_json)
 
-print(sys.argv[2:])
-folder_meta_hash = sys.argv[1]
-folder_meta_json = open('pc1/meta/%s' % folder_meta_hash, 'rb').read()
-folder_meta_data = json.loads(folder_meta_json)
+    print('folder_meta_hash', folder_meta_hash, len(folder_meta_json))
+    pprint.pprint(folder_meta_data)
 
-print('folder_meta_hash', folder_meta_hash, len(folder_meta_json))
-pprint.pprint(folder_meta_data)
+    chunks_to_move = {}
+    for file_meta_data in folder_meta_data['items']:
+        # print(file_meta_data)
+        chunks = file_meta_data[4]
+        # print(chunks)
+        for chunk in chunks:
+            device = chunk[2]
+            chunks_to_move.setdefault(device, set())
+            chunks_to_move[device].add(tuple(chunk))
+    print('chunks_to_move fill')
+    pprint.pprint(chunks_to_move)
 
-chunks_to_move = {}
-for file_meta_data in folder_meta_data['items']:
-    # print(file_meta_data)
-    chunks = file_meta_data[4]
-    # print(chunks)
-    for chunk in chunks:
-        device = chunk[2]
+    new_group0_quota_left = []
+    for device, quota in enumerate(new_group0_quota):
         chunks_to_move.setdefault(device, set())
-        chunks_to_move[device].add(tuple(chunk))
-pprint.pprint(chunks_to_move)
+        if device < group0_device_no:
+            chunks_to_stay = set()
+            for chunk in chunks_to_move[device]:
+                # chunk_hash = chunk[0]
+                chunk_size = chunk[1]
+                if quota >= chunk_size:
+                    quota -= chunk_size
+                    # print(quota)
+                    chunks_to_stay.add(chunk)
+            chunks_to_move[device] -= chunks_to_stay
+            # pprint.pprint(chunks_to_stay)
+        new_group0_quota_left.append(quota)
+    print('chunks_to_move stay')
+    pprint.pprint(chunks_to_move)
 
-for device, quota in enumerate(new_group0_quota):
-    chunks_to_move.setdefault(device, set())
-    if device < group0_device_no:
-        chunks_to_stay = set()
-        for chunk in chunks_to_move[device]:
-            # chunk_hash = chunk[0]
-            chunk_size = chunk[1]
-            if quota >= chunk_size:
-                quota -= chunk_size
-                # print(quota)
-                chunks_to_stay.add(chunk)
-        chunks_to_move[device] -= chunks_to_stay
-        # pprint.pprint(chunks_to_stay)
-pprint.pprint(chunks_to_move)
-     
+    chunks_to_move, new_group0_quota_left = chunks_to_partition(chunks_to_move[0], new_group0_quota_left)
+    print('chunks_to_move')
+    pprint.pprint(chunks_to_move)
+
+    # folder_meta_data_items = []
+    for file_meta_data in folder_meta_data['items']:
+        # print(file_meta_data)
+        chunks = file_meta_data[4]
+        # print(chunks)
+        new_chunks = []
+        for chunk_hash, chunk_size, device in chunks:
+            chunk = tuple([chunk_hash, chunk_size, device])
+            if chunk in chunks_to_move:
+                new_chunks.append([chunk_hash, chunk_size, chunks_to_move[chunk]])
+            else:
+                new_chunks.append([chunk_hash, chunk_size, device])
+        file_meta_data[4] = new_chunks
+        # folder_meta_data_items.append(file_meta_data)
+    # folder_meta_data['items'] = folder_meta_data_items
+    pprint.pprint(folder_meta_data)
+    folder_meta_json = json.dumps(folder_meta_data).encode()
+    folder_meta_hash = hashlib.sha256(folder_meta_json).hexdigest()
+    with open('pc1/meta/%s' % folder_meta_hash, 'wb') as f:
+        f.write(folder_meta_json)
+    print('new folder_meta_hash', folder_meta_hash, len(folder_meta_json))
+
+
+    print('group0_quota         ', group0_quota)
+    print('new_group0_quota     ', new_group0_quota)
+    print('new_group0_quota_left', new_group0_quota_left)
+
