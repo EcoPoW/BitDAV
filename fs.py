66351f8a56e3b37c836a3621b99063e2070b24b9
KJ <kernel1983@gmail.com>
2021-04-02 23:37:24 +0800

    quick fix

--- fs.py
+++ fs.py
@@ -76,7 +76,7 @@ class ListFilesHandler(tornado.web.RequestHandler):
         folder_meta_hash = folders.get(folder_name, '')
 
         storages = get_storages()
-        for storage_name, storage_payload in storages:
+        for storage_name, storage_payload in storages.items():
             storage_path = storage_payload[0]
             node_name = storage_payload[1]
             if node_name == chain.current_name:
